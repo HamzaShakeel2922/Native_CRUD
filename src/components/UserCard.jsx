9a2eb64fbdb37235f593022471931c2867f66067
Hamza Shakeel <hamzashakeel2922@gmail.com>
2024-03-05 00:05:32 +0500

    final refactoring

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -134,6 +134,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 20,
     borderRadius: 20,
+    marginVertical: 10,
   },
   imageContainer: {
     justifyContent: 'center',
